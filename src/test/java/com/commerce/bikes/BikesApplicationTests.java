056cd761ea5bcc4babf700b0453ad62b4dd2a669
Silvio Maia <ingles.silvio@gmail.com>
2023-08-09 14:01:16 -0300

    added everything relevant to bike project

---
+++ src/test/java/com/commerce/bikes/BikesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commerce.bikes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
